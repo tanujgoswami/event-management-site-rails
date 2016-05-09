c489057eb7f0fbc129c593691947dbbfbd48ac4f
Pietro Martini <pmartini.crotti@gmail.com>
2016-05-09 15:09:01 -0400

    write tag counts method in event controller in anticipation of tag cloud

--- event-site/app/models/event.rb
+++ event-site/app/models/event.rb
@@ -6,6 +6,13 @@ class Event < ActiveRecord::Base
   has_many :taggings
   has_many :tags, through: :taggings
 
+  def self.tag_counts
+    Tag.select("tags.name, count(taggings.tag_id) as count").
+    joins(:taggings).group("taggings.tag_id")
+  end
+
+  end
+
   def self.tagged_with(name)
     Tag.find_by_name!(name).events
   end
