b7561fbafa16c48111344f14ccb8a3a9a9fd546e
Pietro Martini <pmartini.crotti@gmail.com>
2016-05-09 11:34:11 -0400

    remove default comments from events conroller

--- event-site/app/controllers/events_controller.rb
+++ event-site/app/controllers/events_controller.rb
@@ -2,28 +2,20 @@ class EventsController < ApplicationController
   before_filter :authenticate_user!
   before_action :set_event, only: [:show, :edit, :update, :destroy]
 
-  # GET /events
-  # GET /events.json
   def index
     @events = Event.all
   end
 
-  # GET /events/1
-  # GET /events/1.json
   def show
   end
 
-  # GET /events/new
   def new
     @event = Event.new
   end
 
-  # GET /events/1/edit
   def edit
   end
 
-  # POST /events
-  # POST /events.json
   def create
     @event = Event.new(event_params)
 
@@ -38,8 +30,6 @@ class EventsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /events/1
-  # PATCH/PUT /events/1.json
   def update
     respond_to do |format|
       if @event.update(event_params)
@@ -52,8 +42,6 @@ class EventsController < ApplicationController
     end
   end
 
-  # DELETE /events/1
-  # DELETE /events/1.json
   def destroy
     @event.destroy
     respond_to do |format|
@@ -69,15 +57,13 @@ class EventsController < ApplicationController
     if @event.organizer_id != current_user.id
       redirect_to events_path
       flash[:notice] = "You do not have the permission to either edit or delete this event posting"
-    end 
+    end
   end
 
-    # Use callbacks to share common setup or constraints between actions.
     def set_event
       @event = Event.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
       params.require(:event).permit(:title, :start_date, :end_date, :location, :agenda, :address, :organizer_id)
     end
