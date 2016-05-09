c7f36d23b7064b963c17054450d55638b6687077
Pietro Martini <pmartini.crotti@gmail.com>
2016-05-09 15:27:56 -0400

    define tag sizes in tags.scss, compose method in application helper to determine tag size based on popularity

--- event-site/app/helpers/application_helper.rb
+++ event-site/app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def tag_cloud(tags, classes)
+    max_tag = tags.sort_by(&:length).last
+    tags.each do |tag|
+      index = tag.length / max_tag.length * (classes.length - 1)
+      yield(tag, classes[index.round])
+    end
+  end
+
 end
