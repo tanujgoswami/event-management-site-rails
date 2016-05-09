48cbdd8d5842f668966e4202397453bcfbe5a83d
Pietro Martini <pmartini.crotti@gmail.com>
2016-05-09 11:36:28 -0400

    apply event_owner! as a before action for edit, update, destroy in events controller

--- event-site/app/controllers/events_controller.rb
+++ event-site/app/controllers/events_controller.rb
@@ -1,5 +1,6 @@
 class EventsController < ApplicationController
   before_filter :authenticate_user!
+  before_action :event_owner!, only: [:edit, :update, :destroy]
   before_action :set_event, only: [:show, :edit, :update, :destroy]
 
   def index
