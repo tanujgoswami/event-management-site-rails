e0fa3d60aa14dd8b561eaeb3356c2f90bc7c55df
Pietro Martini <pmartini.crotti@gmail.com>
2016-05-09 13:41:24 -0400

    define event_tag_strip method to remove whitespace from tag entries

--- event-site/app/models/event.rb
+++ event-site/app/models/event.rb
@@ -5,4 +5,10 @@ class Event < ActiveRecord::Base
   belongs_to :organizers, class_name: "User"
   has_many :taggings
   has_many :tags, through: :taggings
+
+  def event_tag_strip=(names)
+    self.tags = names.split(",").map do |t|
+      Tag.where(name: t.strip).first_or_create!
+    end
+  end
 end
