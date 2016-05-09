573f4dbe2f93614f889d76b6ae67e0635ee8afac
Pietro Martini <pmartini.crotti@gmail.com>
2016-05-09 19:39:24 -0400

    fix bug involving tag links

--- event-site/app/helpers/application_helper.rb
+++ event-site/app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
 
   def tag_cloud(tags, classes)
-    max_tag = tags.sort_by(&:length).last
+    max_tag = tags.sort_by(&:count).last
     tags.each do |tag|
-      index = tag.length / max_tag.length * (classes.length - 1)
+      index = tag.count / max_tag.count * (classes.count - 1)
       yield(tag, classes[index.round])
     end
   end
