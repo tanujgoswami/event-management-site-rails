84b3e65de948722bd954dd91afcd387574475954
Pietro Martini <pmartini.crotti@gmail.com>
2016-05-13 13:35:23 -0400

    compose build for email search method

--- event-site/app/models/event_search_term.rb
+++ event-site/app/models/event_search_term.rb
@@ -12,6 +12,12 @@ class EventSearchTerm
     end
   end
 
+  def build_for_email_search(search_term)
+    @where_clause << case_insensitive_search(:email)
+    @where_args[:email] = search_term
+    @order = "email asc"
+  end
+
   def build_for_title_search(search_term)
     @where_clause << case_insensitive_search(:title)
     @where_args[:title] = starts_with(search_term)
