f0e3014f1f8028ca40042dee06e1f70862a97c3e
Pietro Martini <pmartini.crotti@gmail.com>
2016-05-13 13:29:05 -0400

    define initialize method for event search term, compose component methods for title search

---
+++ event-site/app/models/event_search_term.rb
@@ -0,0 +1,28 @@
+class EventSearchTerm
+  attr_reader :where_clause, :where_args, :order
+
+  def initialize(search_term)
+    search_term = search_term.downcase
+    @where_clause = ""
+    @where_args = {}
+    if search_term =~ /@/
+      build_for_email_search(search_term)
+    else
+      build_for_title_search(search_term)
+    end
+  end
+
+  def build_for_title_search(search_term)
+    @where_clause << case_insensitive_search(:title)
+    @where_args[:title] = starts_with(search_term)
+    @order = "title asc"
+  end
+
+  def starts_with(search_term)
+    search_term + "%"
+  end
+
+  def case_insensitive_search(field_name)
+    "lower(#{field_name}) like :#{field_name}"
+  end
+end
