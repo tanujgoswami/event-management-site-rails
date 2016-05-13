ea33fd56f5e40f5121855718060a3c6424997938
Pietro Martini <pmartini.crotti@gmail.com>
2016-05-12 21:15:51 -0400

    add valid email constraint regex

--- event-site/db/migrate/20160513010824_add_email_validations_to_user.rb
+++ event-site/db/migrate/20160513010824_add_email_validations_to_user.rb
@@ -1,4 +1,20 @@
 class AddEmailValidationsToUser < ActiveRecord::Migration
-  def change
+  def up
+    execute %{
+      ALTER TABLE
+        users
+      ADD CONSTRAINT
+        email_must_be_valid
+      CHECK ( email ~* '/\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i )
+    }
+  end
+
+  def down
+    execute %{
+      ALTER TABLE
+        users
+      DROP CONSTRAINT
+        email_must_be_valid
+    }
   end
 end
